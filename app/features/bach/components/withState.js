35d0eba6141e14122223fb891b5be979e09cbe21
Josh Gretz <jgretz@truefit.io>
2019-05-27 22:11:43 -0400

    update withState to show function example

--- app/features/bach/components/withState.js
+++ app/features/bach/components/withState.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import {compose, withState} from '@truefit/bach';
 
-const Component = ({count, setCount}) => (
+const Component = ({count, setCount, count2, setCount2}) => (
   <div>
     <h1>With State</h1>
     <div>
       <h2>Count: {count}</h2>
       <button onClick={() => setCount(count + 1)}>Increment</button>
     </div>
+    <div>
+      <h2>Count 2: {count2}</h2>
+      <button onClick={() => setCount2(count2 + 1)}>Increment</button>
+    </div>
   </div>
 );
 
-export default compose(withState('count', 'setCount', 0))(Component);
+const ComposedComponent = compose(
+  withState('count', 'setCount', 0),
+  withState('count2', 'setCount2', ({initialCount}) => initialCount),
+)(Component);
+
+export default () => <ComposedComponent initialCount={10} />;
